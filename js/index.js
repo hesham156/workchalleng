6c5faf9cb81ee176493b07e7ba69ace098f45ce2
hesham156 <h456ad@gmail.com>
2023-02-06 16:52:24 +0200

    work

--- js/index.js
+++ js/index.js
@@ -1,14 +1,46 @@
 const steps =Array.from(document.querySelectorAll('.step'))
 const cl =Array.from(document.querySelectorAll('.cl'))
-console.log(cl)
-cl.forEach((elm)=>{
-    elm.onclick=()=>{
-        for(let i = 0 ; i<cl.length;i++){
-              cl[i].classList.remove('activate')
-              steps[i].classList.remove('sactivate')
+const btnBack = document.querySelector('.back');
+const btnNext = document.querySelector('.next');
+const Csteps = (cle,sts,theElm)=>{
+    for(let i = 0 ; i<cl.length;i++){
+        cle[i].classList.remove('activate')
+        sts[i].classList.remove('sactivate')
 
-        }
-        elm.classList.add('activate')
-        steps[cl.indexOf(elm)].classList.add('sactivate')
+  }
+  theElm.classList.add('activate')
+  sts[cle.indexOf(theElm)].classList.add('sactivate')
+
+}
+cl.forEach((telm)=>{
+    telm.onclick=()=>{
+        Csteps(cl,steps,telm)
     }
 })
+
+addEventListener('load',()=>{
+    if(steps[0].classList.contains('sactivate')){
+        btnBack.style.display="none"
+        }else{
+            btnBack.style.display="block"
+        }
+
+})
+addEventListener('click',()=>{
+    if(steps[0].classList.contains('sactivate')){
+        btnBack.style.display="none"
+        }else{
+            btnBack.style.display="block"
+        }
+
+})
+btnNext.onclick=()=>{
+    const activateelm = document.querySelector('.sactivate')
+     let index = steps.indexOf(activateelm)
+     Csteps(cl,steps,cl[index+1<=3?index+1:index])
+}
+btnBack.onclick=()=>{
+    const activateelm = document.querySelector('.sactivate')
+     let index = steps.indexOf(activateelm)
+     Csteps(cl,steps,cl[index-1])
+}
